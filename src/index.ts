2e54f1bc268a6f584ff40b62f90b923672c9b19b
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-02-10 21:58:20 +0800

    setup the server

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import {MikroORM} from "@mikro-orm/core";
+import microConfig from './mikro-orm.config';
+import express from 'express';
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
+
+const main = async () => {
+    await MikroORM.init(microConfig);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        })
+    })
+
+    apolloServer.applyMiddleware({app});
+
+    app.listen(4000, () => {
+        console.log('server load in localhost:4000');
+    })
+}
+
+main().catch((err) => console.error(err));
