2d9b089b7590058b667261c84be61119c5793e66
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-02-10 22:32:18 +0800

    crud using mikro-orm

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
+import "reflect-metadata";
 import {MikroORM} from "@mikro-orm/core";
 import microConfig from './mikro-orm.config';
 import express from 'express';
 import {ApolloServer} from "apollo-server-express";
 import {buildSchema} from "type-graphql";
-import {HelloResolver} from "./resolvers/hello";
+import {PostResolver} from "./resolvers/post";
 
 const main = async () => {
-    await MikroORM.init(microConfig);
+    const orm = await MikroORM.init(microConfig);
     const app = express();
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [HelloResolver],
+            resolvers: [PostResolver],
             validate: false,
-        })
+        }),
+        context: () => ({em: orm.em})
     })
 
     apolloServer.applyMiddleware({app});
